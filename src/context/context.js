ee8891909bec01d1f7b288fca9a8e6e115d7d25c
mastanpatel <Bushra.mateen95@outlook.com>
2022-01-18 20:59:56 +0530

    loading spinner

--- src/context/context.js
+++ src/context/context.js
@@ -14,7 +14,55 @@ const GithubProvider = ({children}) => {
     const [githubUser,setGithubUser]= useState(mockUser);
     const [repos,setRepos]=useState(mockRepos);
     const [followers,setFollowers]=useState(mockFollowers);
-    return(<GithubContext.Provider value={{githubUser, repos,followers}}>{children}</GithubContext.Provider>
+    const [requests,setRequests]=useState(0);
+    const [isLoading,setIsLoading]=useState(false);
+
+    const [error,setError]= useState({show:false,msg:""})
+
+    const searchGithubUser =async (user) => {
+        toggleError();
+        setIsLoading(true)
+        const response = await axios(`${rootUrl}/users/${user}`)
+        .catch((err) => 
+        console.log(err) )
+    
+    console.log(response)
+    if(response) {
+        setGithubUser(response.data)
+    }else {
+        toggleError(true,'there is no user with that username')
+    }
+    checkRequests()
+    setIsLoading(false)
+}
+
+    const checkRequests = ()=> {
+        axios(`${rootUrl}/rate_limit`)
+        .then((data)=>{
+            let {
+                rate: { remaining },
+            } = data;
+          
+            setRequests(remaining);
+            if(remaining === 0){
+                toggleError(true,'sorry, you have exceeded your hourly rate limit!');
+
+            }
+        })
+        .catch((err) => console.log(err));
+
+        };
+    function toggleError(show = false,msg = '') {
+        setError({show,msg})
+    }
+    //error
+    useEffect(checkRequests, []);
+    return(
+    <GithubContext.Provider value={{githubUser, repos,followers,
+    requests, error,searchGithubUser,isLoading}
+}>
+    {children}
+    </GithubContext.Provider>
     )
 }
 

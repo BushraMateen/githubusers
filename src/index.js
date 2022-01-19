bbd1cf0d7e7b12c95bbdf7a5756b4477c427f6fe
mastanpatel <Bushra.mateen95@outlook.com>
2022-01-19 16:39:41 +0530

    login/logout

--- src/index.js
+++ src/index.js
@@ -5,12 +5,21 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { GithubContext, GithubProvider } from './context/context';
 import { Auth0Provider } from '@auth0/auth0-react';
-
+// dev-i-m6fh-v.us.auth0.com
+// kRAUEf4nry1hfaC0uzQtoiOyIqBEjJm2
 ReactDOM.render(
   <React.StrictMode>
+    <Auth0Provider
+      domain= "dev-i-m6fh-v.us.auth0.com"
+      clientId='kRAUEf4nry1hfaC0uzQtoiOyIqBEjJm2'
+      redirectUri={window.location.origin}
+      >
+        
+  
     <GithubProvider>
     <App />
     </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
